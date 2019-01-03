23abdf6931d9eda624f4898058772edc73a563e6
crisovik <criss00zx@outlook.com>
2019-01-03 13:35:47 -0600

    Configuración inicial

---
+++ application/views/layout/head.php
@@ -0,0 +1,24 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title><?php echo $title; ?></title>
+  <meta name="description" content="Invitation manager">
+  <meta name="author" content="crisovik">
+
+  <!-- Styles -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+  <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/custom.css">
+
+</head>
+<body>
+<div id="container">
+
+  <div class="row">
+    <div class="offset-md-3 col-md-3">
+      <h1>Oliiiiii</h1>
+    </div>
+  </div>
